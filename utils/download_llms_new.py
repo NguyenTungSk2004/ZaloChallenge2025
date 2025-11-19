046294ae453e55fa5650f3288e244384e84ccc56
tungsk <skfreelancer2004@gmail.com>
2025-11-19 08:33:27 +0700

    udpate train yolo

--- utils/download_llms_new.py
+++ utils/download_llms_new.py
@@ -1,13 +1,13 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="Qwen/Qwen3-8B",
-    local_dir="./models/Qwen/Qwen3-8B",
+    repo_id="Qwen/Qwen3-4B",
+    local_dir="./models/Qwen/Qwen3-4B",
     local_dir_use_symlinks=False
 )
 
 snapshot_download(
-    repo_id="Qwen/Qwen3-4B",
-    local_dir="./models/Qwen/Qwen3-4B",
+    repo_id="Qwen/Qwen2-VL-2B-Instruct",
+    local_dir="./models/Qwen/Qwen2-VL-2B-Instruct",
     local_dir_use_symlinks=False
 )
