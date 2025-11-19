635062910b54e847a47ad99386b519844424841d
tungsk <skfreelancer2004@gmail.com>
2025-11-19 17:34:20 +0700

    udpate Qwen/Qwen3-VL-2B-Instruct

--- utils/download_models_new.py
+++ utils/download_models_new.py
@@ -19,7 +19,7 @@ snapshot_download(
 )
 
 snapshot_download(
-    repo_id="Qwen/Qwen2-VL-2B-Instruct",
-    local_dir="./models/Qwen/Qwen2-VL-2B-Instruct",
+    repo_id="Qwen/Qwen3-VL-2B-Instruct",
+    local_dir="./models/Qwen/Qwen3-VL-2B-Instruct",
     local_dir_use_symlinks=False
 )
